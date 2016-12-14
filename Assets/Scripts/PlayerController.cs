9ea1cc660a667cb9a310b2d48bbaa82812571c8b
Jason Romero <codenamejason@outlook.com>
2016-12-14 15:23:02 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlyerController : MonoBehaviour
+{
+    // Create object reference variable
+    private Rigidbody rb;
+
+    void Start()
+    {
+        // gets the rigidbody component
+        rb.GetComponent<Rigidbody>();
+    }
+
+	// Use this for update
+	void FixedUpdate()
+	{
+        // Horizontal and Vertical Inputs from Player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement);
+
+	}
+}
